67b83e45ca4aaa3a9372e29585d3ef0771b650a7
edamerveatalay <atalayedaatalay@gmail.com>
2025-11-05 10:25:05 +0300

    like modeli oluşturuldu

---
+++ app/models/models_like.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import Field, Relationship, SQLModel
+
+from app.models.models_blog import Blog
+from app.models.models_user import User
+
+
+class Like(SQLModel, table=True):
+    __tablename__ = "likes"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="users.id")
+    blog_id: int = Field(foreign_key="blogs.id")
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+    user: Optional["User"] = Relationship(back_populates="likes")
+    blog: Optional["Blog"] = Relationship(back_populates="likes")
